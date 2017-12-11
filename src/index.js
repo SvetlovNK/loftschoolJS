dff2b16b93cb71385e5431a8d18ab2aeff67ffba
Nickolay Svetlov <ns@idaproject.com>
2017-12-11 21:56:46 +0300

    end part I

--- src/index.js
+++ src/index.js
@@ -42,7 +42,50 @@ function createCookie(name, value) {
  * @param name - имя
  */
 function deleteCookie(name) {
+    setCookie(name, null, { expires: -1 });
 }
+
+function setCookie(name, value, options) {
+    options = options || {};
+
+    let expires = options.expires;
+
+    if (typeof expires === 'number' && expires) {
+        let date = new Date();
+
+        date.setTime(date.getTime() + expires * 1000);
+        expires = options.expires = date;
+    }
+
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    let updatedCookie = name + '=' + value;
+
+    for (let propName in options) {
+        let propValue;
+
+        updatedCookie += '; ' + propName;
+        propValue = options[propName];
+
+        if (propValue !== true) {
+            updatedCookie += '=' + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+/**
+function getCookie(name) {
+    let regEx = new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)");
+    let matches = document.cookie.match(regEx);
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+ */
 
 export {
     createWindow,
